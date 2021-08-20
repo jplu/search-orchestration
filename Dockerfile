5ebc61921d3d03f4c57860d57170edbd16ce67e3
jplu <plu.julien@gmail.com>
2021-08-20 13:23:43 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,25 @@
+FROM python:3.8-alpine
+
+RUN addgroup -S -g 1000 search && \
+    adduser -S -u 1000 -h /home/search -s /bin/bash -g search search
+
+RUN apk add gcc g++ linux-headers libffi-dev make rust cargo
+
+RUN pip install --no-cache-dir -U nvidia-pyindex \
+    && pip install --no-cache-dir -U gunicorn prometheus-fastapi-instrumentator uvicorn transformers tritonclient[all] fastapi elasticsearch==7.13.1
+
+COPY ./gunicorn_conf.py /gunicorn_conf.py
+COPY ./start.sh /start.sh
+COPY ./app /app
+
+RUN chmod +x /start.sh
+
+WORKDIR /app/
+
+ENV PYTHONPATH=/app
+
+EXPOSE 80
+
+USER search
+
+CMD ["/start.sh"]
---
+++ app/main.py
@@ -0,0 +1,189 @@
+import logging
+import os
+import sys
+
+from typing import List
+
+import grpc
+import numpy as np
+import tritonclient.grpc as grpcclient
+
+from elasticsearch import Elasticsearch
+from fastapi import FastAPI
+from fastapi.logger import logger as fastapi_logger
+from prometheus_fastapi_instrumentator import Instrumentator
+from pydantic import BaseModel, HttpUrl
+from starlette.responses import Response, JSONResponse
+from starlette.status import HTTP_200_OK
+from transformers import DistilBertTokenizer
+
+import faiss_pb2_grpc
+import faiss_pb2
+
+
+class QueryItem(BaseModel):
+    query: str
+
+
+class DocumentItem(BaseModel):
+    url: HttpUrl
+    context: str
+    pid: int
+    title: str
+    id: int
+
+
+class OrchestrationException(Exception):
+    def __init__(self, query: str, message: str, from_svc: str):
+        self.query = query
+        self.message = message
+        self.from_svc = from_svc
+
+
+es_login = os.getenv("ES_LOGIN")
+es_password = os.getenv("ES_PASSWORD")
+es_host = os.getenv("ES_HOST")
+grpc_faiss_host = os.getenv("FAISS_GRPC_HOST")
+grpc_triton_host = os.getenv("TRITON_GRPC_HOST")
+
+es_client = Elasticsearch(
+    [es_host],
+    http_auth=(es_login, es_password),
+    scheme="https",
+    port=443,
+    timeout=300,
+    retry_on_timeout=True,
+    max_retries=10,
+    maxsize=25,
+)
+
+tokenizer = DistilBertTokenizer.from_pretrained('sentence-transformers/msmarco-distilbert-base-v3', use_fast=True)
+
+try:
+    faiss_channel = grpc.insecure_channel(f'{grpc_faiss_host}:8081')
+    stub = faiss_pb2_grpc.FaissServiceStub(faiss_channel)
+except Exception as e:
+    print("FAISS channel creation failed: " + str(e))
+    sys.exit()
+
+model_name = "encode_onnx"
+
+try:
+  triton_client = grpcclient.InferenceServerClient(
+    url=f"{grpc_triton_host}:8001"
+  )
+except Exception as e:
+    print("Triton channel creation failed: " + str(e))
+    sys.exit()
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+gunicorn_error_logger = logging.getLogger("gunicorn.error")
+gunicorn_logger = logging.getLogger("gunicorn")
+uvicorn_access_logger = logging.getLogger("uvicorn.access")
+
+uvicorn_access_logger.handlers = gunicorn_error_logger.handlers
+fastapi_logger.setLevel(gunicorn_logger.level)
+
+
+def encode_with_triton(query):
+    inputs = []
+    outputs = []
+    model_input = tokenizer.encode_plus(query, truncation=True)
+    input_ids = np.expand_dims(model_input["input_ids"], axis=0)
+    attention_mask = np.expand_dims(model_input["attention_mask"], axis=0)
+    
+    inputs.append(grpcclient.InferInput('input_ids', [1, len(model_input["input_ids"])], "INT64"))
+    inputs.append(grpcclient.InferInput('attention_mask', [1, len(model_input["attention_mask"])], "INT64"))
+
+    inputs[0].set_data_from_numpy(input_ids)
+    inputs[1].set_data_from_numpy(attention_mask)
+
+    outputs.append(grpcclient.InferRequestedOutput('sentence_embedding'))
+
+    results = triton_client.infer(
+        model_name=model_name,
+        inputs=inputs,
+        outputs=outputs,
+        headers={'test': '1'}
+    )
+
+    return results.as_numpy('sentence_embedding')[0]
+
+
+def nearest_documents_faiss(encoded_query, k):
+    faiss_request = faiss_pb2.SearchRequest()
+    faiss_request.top_k = k
+    faiss_request.vector.float_val.extend(encoded_query)
+    response = stub.Search.future(faiss_request, 90)
+    result = response.result()
+    ids = []
+
+    for res in result.neighbors:
+        ids.append(res.id)
+
+    return ids
+
+
+def get_es_documents(ids):
+    documents = []
+
+    for id in ids:
+        doc = es_client.get(index="en_wikipedia", id=id)
+        documents.append(doc['_source'])
+
+    return documents
+
+@app.exception_handler(OrchestrationException)
+async def orchestration_exception_handler(request, exc):
+    fastapi_logger.error(f"""
+        ======================
+        Query
+        {str(exc.query)}
+        Exception
+        {str(exc.message)}
+        From
+        {str(exc.from_svc)}
+        ======================
+    """)
+    
+    return JSONResponse(
+        status_code=500,
+        content={"detail": "Internal server error"},
+    )
+
+@app.post('/search', response_model=List[DocumentItem])
+async def search(query: QueryItem, k: int = 5):
+    q = query.query
+    
+    try:
+        encoded_query = encode_with_triton(q)
+    except Exception as e:
+        raise OrchestrationException(query=query, message=str(e), from_svc="Triton")
+    
+    try:
+        nearest_document_ids = nearest_documents_faiss(encoded_query, k)
+    except Exception as e:
+        raise OrchestrationException(query=query, message=str(e), from_svc="FAISS")
+    
+    try:
+        documents = get_es_documents(nearest_document_ids)
+    except Exception as e:
+        raise OrchestrationException(query=query, message=str(e), from_svc="Elasticsearch")
+
+    return documents
+
+
+@app.get('/health')
+async def health():
+    return Response(status_code=HTTP_200_OK)
+
+@app.get('/ready')
+async def ready():
+    return Response(status_code=HTTP_200_OK)
+
+@app.get('/')
+async def root():
+    return Response(status_code=HTTP_200_OK)
